631de704c2f812518971d51e9a03f5ecaa03bd99
arihidayatm <muhamadarihidayat@gmail.com>
2024-07-08 09:58:21 +0700

    first commit

---
+++ lib/presentation/home/pages/setting_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Setting Page'),
+      ),
+      body: Center(
+          child: BlocConsumer<LogoutBloc, LogoutState>(
+        listener: (context, state) {
+          state.maybeMap(
+            orElse: () {},
+            success: (_) {
+              context.pushReplacement(const LoginPage());
+            },
+            error: (value) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(value.error),
+                  backgroundColor: AppColors.red,
+                ),
+              );
+            },
+          );
+        },
+        builder: (context, state) {
+          return state.maybeWhen(
+            orElse: () {
+              return Button.filled(
+                onPressed: () {
+                  context.read<LogoutBloc>().add(const LogoutEvent.logout());
+                },
+                label: 'Logout',
+              );
+            },
+            loading: () {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          );
+        },
+      )),
+    );
+  }
+}
